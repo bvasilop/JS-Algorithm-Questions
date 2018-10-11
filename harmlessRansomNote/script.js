a189b92db5df8cf6626acbc3bf31fe15b1bbcdb7
bvasilop <bvasilop@gmail.com>
2018-10-10 20:35:46 -0700

    added Big O notation content for ransom Note

---
+++ harmlessRansomNote/script.js
@@ -0,0 +1,12 @@
+/*****
+ * Big O Notation
+ *  */
+
+// Is a notation used to classify how scalable an algorithm or function is and it allows us to estimate
+    // the worst case run-time of an algorithm, or how long it takes the algorithm to complete based on the input size
+        // Big O informs us of how much slower an algorithm will run if its input size grows. If the input size
+            // gets larger (array of 100 elements instead of array of 50), will the run-time of the function stay
+                // the same ? Will the run-time get proportionally larger as the input size increases ? Will the
+                    // run-time get exponentially larger or will the run-time change in some other way ?
+                        // Big O describes how performant a function or algorithm is.
+
